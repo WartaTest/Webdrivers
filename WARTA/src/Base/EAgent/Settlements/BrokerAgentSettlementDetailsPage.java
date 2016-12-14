7ba0c65f4dfa1c51ca2b2684527cfcc71045b279
WARTA\JF230163 <akupczyk.icm@gmail.com>
2016-12-14 15:56:27 +0100

    dodaje rozliczenia 2 pierwsze strony, prawie skończone

---
+++ WARTA/src/Base/EAgent/Settlements/BrokerAgentSettlementDetailsPage.java
@@ -0,0 +1,15 @@
+package Base.EAgent.Settlements;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import Base.EAgent.BrowserFactory;
+
+public class BrokerAgentSettlementDetailsPage {
+	private WebDriver driver = BrowserFactory.getBrowser("Firefox");
+	
+	
+	}
+
+
--- WARTA/src/Tests/Settlements/Settlement.java
+++ WARTA/src/Tests/Settlements/Settlement.java
@@ -1,16 +1,21 @@
 package Tests.Settlements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.junit.Test;
 
 import Base.EAgent.BrowserFactory;
-import Base.EAgent.Settlements;
 import Base.EAgent.StartPage;
 import Base.EAgent.Login.Login;
 import Base.EAgent.Login.Logins.username1;
+import Base.EAgent.Settlements.BrokerAgentSettlementPage;
+import Base.EAgent.Settlements.Settlements;
+
+import java.lang.System;
 
 public class Settlement {
-	
+	private WebDriver driver = BrowserFactory.getBrowser("Firefox");
 	@Test
 	public void Test() throws InterruptedException{
 		
@@ -24,6 +29,29 @@ public class Settlement {
 		settlements.settlementsSearchInputFillUp("5504071");
 		settlements.firstElementOfSettlementsSearchClick();
 		
-	}
+		String role;
+		role = "Agent:";
+		String id;
+		id = " " + "GDAH00230000";
+		BrokerAgentSettlementPage brokerAgentSettlementPage = new BrokerAgentSettlementPage();
+		brokerAgentSettlementPage.roleAndIdCheck((role + id));
+		
+		
+		WebElement element;
+		element = driver.findElement(By.className("tile-settlement-summary__agent-context__agent-fullname__value"));
+		String nameAndCompanyFullName;
+		nameAndCompanyFullName = "TOMASZ ZYGMUNT HERSTOWSKI NOWA GENERACJA UBEZPIECZEÑ";
+		settlements.waitForTextToAppear(nameAndCompanyFullName, element);
+		brokerAgentSettlementPage.nameAndComapnyFullNameCheck(nameAndCompanyFullName);
+		
+		Thread.sleep(6000);
+		
 
+		brokerAgentSettlementPage.settlementClickOn("RSP/16414986/16/1716");
+
+		//brokerAgentSettlementPage.periodCheckValue("01-09-2016 - 15-09-2016");
+		
+			
+		//driver.quit();
+		}
 }
